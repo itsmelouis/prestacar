e9e6d58b0a10eaba35dbda36de6b15c8eef9158e
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-01-17 23:28:38 +0100

    Update src/pages/Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -48,7 +48,7 @@ const images = [
   { id: 3, src: getVehiculeImage("lincoln_towncar/limo.jpg") },
   { id: 4, src: getVehiculeImage("cadillac_escalade/cadillac14.jpg") },
   {
-    id: 4,
+    id: 5,
     src: getVehiculeImage("bentley_continental_gt_sport/bentley-2-1.jpg"),
   },
   { id: 4, src: getVehiculeImage("rolls_royce_phantom/rolls.jpg") },
