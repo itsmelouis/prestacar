0db3cb3770e8cd68a86e0259f0d0d55d56cefa63
theomrn <58818965+theomrn@users.noreply.github.com>
2024-02-01 21:19:10 +0100

    Add files via upload

---
+++ src/pages/Cars/2cv.vue
@@ -0,0 +1,123 @@
+<template>
+  <div ref="el" class="my-20">
+    <div class="my-10 mx-10 sm:my-20 md:mx-40">
+      <p class="text-base md:text-lg lg:text-xl">
+        La célèbre <span class="font-bold">coccinelle</span> de chez Volkswagen
+        imaginée par Ferdinand Porsche est à votre portée. Ce magnifique
+        véhicule de 1973 s'offre à vous pour tous types d'événements. Souvent
+        associée au Combi, les deux véhicules sont disponibles simultanément à
+        la location.
+        <br />
+        <br />
+        Capacité: 3 passagers. (2 recommandés)
+        <br />
+        <br />
+        Si votre souhait ne se trouve pas dans la grille tarifaire, n'hésitez
+        pas à nous contacter pour obtenir rapidement un devis. Nous saurons nous
+        adapter aisément.
+        <br />
+        <br />
+        Les expositions et visites sont sur devis. L'approche est de 30km autour
+        de Valenciennes, au-delà un supplément sera demandé.
+        <br />
+        <br />
+      </p>
+    </div>
+    <Carousel :items="images" />
+    <div
+        class="flex flex-col md:flex-row md:justify-around items-center mt-20 space-y-10"
+    >
+      <div class="space-y-8">
+        <p class="text-center">
+          Grille Tarifaire <br />(Tous nos tarifs sont dégressifs si vous louez
+          plusieurs véhicules, Tarifs avec Chauffeur)
+        </p>
+        <Table :data="grille_tarifiaire"></Table>
+      </div>
+      <div class="space-y-8">
+        <p class="text-center">Options</p>
+        <Table :data="options"></Table>
+      </div>
+    </div>
+    <div class="flex justify-center items-center mt-20">
+      <router-link
+          to="/contact"
+          class="bg-yellow-400 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full text-center w-44"
+      >Réserver</router-link
+      >
+    </div>
+    <GoTop />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import {
+  getVehiculeImage,
+  getVehiculeImageAvif,
+  getVehiculeImageWebp,
+} from "../../utils/importImages";
+import Carousel from "../../components/Carousel.vue";
+import Table from "../../components/Table.vue";
+import GoTop from "../../components/GoTop.vue";
+
+const el = ref<HTMLDivElement>();
+
+const grille_tarifiaire = [
+  ["Temps", "Prix"],
+  ["Aller Simple", "Sur devis"],
+  ["Aller Retour", "Sur devis"],
+  ["1h", "150€"],
+  ["3h", "300€"],
+  ["Forfait Mariage", "400€"],
+];
+
+const options = [
+  ["Intitulé", "Prix"],
+  ["Photographe", "Sur devis"],
+  ["Fleurs", "Sur devis"],
+];
+
+const images = [
+  {
+    id: 1,
+    src: getVehiculeImage("citroen_2cv/2cv-2.jpg"),
+    avif: getVehiculeImageAvif("citroen_2cv/2cv-2.avif"),
+    webp: getVehiculeImageWebp("citroen_2cv/2cv-2.webp"),
+  },
+  {
+    id: 2,
+    src: getVehiculeImage("citroen_2cv/2cv-3.jpg"),
+    avif: getVehiculeImageAvif("citroen_2cv/2cv-3.avif"),
+    webp: getVehiculeImageWebp("citroen_2cv/2cv-3.webp"),
+  },
+  {
+    id: 3,
+    src: getVehiculeImage("citroen_2cv/2cv-1.jpg"),
+    avif: getVehiculeImageAvif("citroen_2cv/2cv-1.avif"),
+    webp: getVehiculeImageWebp("citroen_2cv/2cv-1.webp"),
+  },
+  {
+    id: 4,
+    src: getVehiculeImage("citroen_2cv/2cv-4.jpg"),
+    avif: getVehiculeImageAvif("citroen_2cv/2cv-4.avif"),
+    webp: getVehiculeImageWebp("citroen_2cv/2cv-4.webp"),
+  },
+  {
+    id: 5,
+    src: getVehiculeImage("citroen_2cv/2cv-5.jpg"),
+    avif: getVehiculeImageAvif("citroen_2cv/2cv-5.avif"),
+    webp: getVehiculeImageWebp("citroen_2cv/2cv-5.webp"),
+  },
+  {
+    id: 7,
+    src: getVehiculeImage("citroen_2cv/2cv-7.jpg"),
+    avif: getVehiculeImageAvif("citroen_2cv/2cv-7.avif"),
+    webp: getVehiculeImageWebp("citroen_2cv/2cv-7.webp"),
+  },
+];
+
+onMounted(() => {
+  window.scrollTo(0, el.value!.offsetTop - 20);
+});
+</script>
