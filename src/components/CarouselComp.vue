9d5f4e25c33b8b4ace5fa5eb70ad26559d1d2461
theomrn <58818965+theomrn@users.noreply.github.com>
2024-10-25 14:44:33 +0200

    feat: add avif and webp to home carousel

--- src/components/CarouselComp.vue
+++ src/components/CarouselComp.vue
@@ -4,7 +4,11 @@
       <Carousel :autoplay="2000" :wrap-around="true">
         <Slide v-for="prop in props.items" :key="prop.id">
           <div class="max-h-80 w-full flex justify-center items-center">
-            <img :src="prop.src" class="w-full h-auto" />
+            <picture>
+                <source :srcset="prop.avif" type="image/avif" />
+                <source :srcset="prop.webp" type="image/webp" />
+                <img :src="prop.src" class="w-full h-auto object-cover" />
+              </picture>
           </div>
         </Slide>
       </Carousel>
@@ -21,6 +25,8 @@ import "vue3-carousel/dist/carousel.css";
 type CarouselImg = {
   id: number;
   src: string;
+  avif:string;
+  webp:string;
 };
 
 const props = defineProps({
